cfaa52e6c40e2f788e63912012f36c2d532813ce
Victor Alves <victoralves@sent.com>
2026-08-31 23:40:19 -0300

    feat: implementa UsuarioDetailsService e JwtAuthenticationFilter

---
+++ sgm/src/main/java/com/sgm/sgm/security/UsuarioDetailsService.java
@@ -0,0 +1,27 @@
+package com.sgm.sgm.security;
+
+import com.sgm.sgm.model.Usuario;
+import com.sgm.sgm.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UsuarioDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new User(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+    }
+}
