6aa17f7e49c6983444b6c66978bb701482939cb9
Victor Alves <victoralves@sent.com>
2026-08-31 22:47:42 -0300

    feat: implementa repository de OrdemManutencao e HistoricoStatus

---
+++ sgm/src/main/java/com/sgm/sgm/repository/HistoricoStatusRepository.java
@@ -0,0 +1,7 @@
+package com.sgm.sgm.repository;
+
+import com.sgm.sgm.model.HistoricoStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoricoStatusRepository extends JpaRepository<HistoricoStatus, Long> {
+}
